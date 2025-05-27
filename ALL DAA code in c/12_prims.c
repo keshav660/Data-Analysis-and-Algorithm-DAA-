07938f4b6f682aec18d310e090a86ca9d60e21fe
keshav660 <keshavbhatt660@gmail.com>
2025-05-27 20:07:02 +0530

    new updated code

--- ALL DAA code in c/12_prims.c
+++ ALL DAA code in c/12_prims.c
@@ -1,35 +1,58 @@
 #include <stdio.h>
 
-int main() {
-    int cost[10][10] = {
-        {999, 2, 999, 6},
-        {2, 999, 3, 8},
-        {999, 3, 999, 999},
-        {6, 8, 999, 999}
-    };
-    int n = 4, visited[10] = {0}, ne = 0, mincost = 0;
-    visited[0] = 1;
+#define INF 999
 
-    while (ne < n - 1) {
-        int min = 999, a = -1, b = -1;
-        for (int i = 0; i < n; i++) {
-            if (visited[i]) {
-                for (int j = 0; j < n; j++) {
-                    if (!visited[j] && cost[i][j] < min) {
+void prim(int n, int cost[10][10]) {
+    int selected[10] = {0}, i, j, u, v, min, edges = 0;
+
+    selected[0] = 1;  // Start from vertex 0
+
+    printf("\nEdges in Minimum Spanning Tree:\n");
+
+    while (edges < n - 1) {
+        min = INF;
+
+        for (i = 0; i < n; i++) {
+            if (selected[i]) {
+                for (j = 0; j < n; j++) {
+                    if (!selected[j] && cost[i][j] < min) {
                         min = cost[i][j];
-                        a = i;
-                        b = j;
+                        u = i;
+                        v = j;
                     }
                 }
             }
         }
-        if (a != -1 && b != -1) {
-            printf("Edge %d: (%d,%d) cost:%d\n", ne + 1, a, b, min);
-            visited[b] = 1;
-            mincost += min;
-            ne++;
-        }
+
+        printf("%d - %d : %d\n", u, v, cost[u][v]);
+        selected[v] = 1;
+        edges++;
     }
-    printf("Minimum cost = %d\n", mincost);
+}
+
+int main() {
+    int n, i, j, cost[10][10];
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter the cost adjacency matrix (use 999 for no edge):\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &cost[i][j]);
+
+    prim(n, cost);
+
     return 0;
 }
+// Enter number of vertices: 4
+// Enter the cost adjacency matrix (use 999 for no edge):
+// 999 2   999 6
+// 2   999 3   8
+// 999 3   999 1
+// 6   8   1   999
+
+// Edges in Minimum Spanning Tree:
+// 2 - 3 : 1
+// 1 - 2 : 3
+// 0 - 1 : 2
